890a7ce80c5aa27a9a3f2eddbceb72f35dd98a26
Valeriy Kovalevskiy <v.kovalevskiy@vironit.co.uk>
2020-09-11 12:18:33 +0300

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Valeriy Kovalevskiy on 9/11/20.
+//  Copyright © 2020 Valeriy Kovalevskiy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
